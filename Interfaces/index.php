bd7821ae3835562946dd1e10a08a72be9ab5612f
thiago <thiago@thiagodsantos.com>
2018-07-09 22:24:49 -0400

    interfaces

---
+++ Interfaces/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<!--
+*        Sample comment
+-->
+<HTML>
+<head>
+    <title>PhpStorm</title>
+</head>
+<body>
+<pre>
+<?php
+
+require_once  'ControleRemoto.php';
+
+$c = new ControleRemoto();
+
+$c->abrirMenu();
+
+$c->maisVolume();
+
+
+?>
+    </pre>
+</body>
+</html>
+

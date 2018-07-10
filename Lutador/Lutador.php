2219dccf073115883246b20bbeb01b3f90916e86
thiago <thiago@thiagodsantos.com>
2018-07-09 23:52:54 -0400

    Interfaces - pardaro, classe e classe principal

---
+++ Lutador/Lutador.php
@@ -0,0 +1,251 @@
+<?php
+
+require  'Padrao.php';
+
+Class Lutador implements Padrao {
+
+    private $nome;
+    private $nacionalidade;
+    private $idade;
+    private $altura;
+    private $peso;
+    private $categoria;
+    private $vitorias;
+    private $derrotas;
+    private $empates;
+
+    /**
+     * Lutador constructor.
+     */
+    public function __construct()
+    {
+
+        $this->nome = 'no';
+        $this->nacionalidade = 'na';
+        $this->idade = 'id';
+        $this->altura = 'al';
+        $this->vitorias = 'vi';
+        $this->derrotas = 'de';
+        $this->empates = 'em';
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * @param string $nome
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNacionalidade()
+    {
+        return $this->nacionalidade;
+    }
+
+    /**
+     * @param string $nacionalidade
+     */
+    public function setNacionalidade($nacionalidade)
+    {
+        $this->nacionalidade = $nacionalidade;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIdade()
+    {
+        return $this->idade;
+    }
+
+    /**
+     * @param string $idade
+     */
+    public function setIdade($idade)
+    {
+        $this->idade = $idade;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAltura()
+    {
+        return $this->altura;
+    }
+
+    /**
+     * @param string $altura
+     */
+    public function setAltura($altura)
+    {
+        $this->altura = $altura;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPeso()
+    {
+        return $this->peso;
+    }
+
+    /**
+     * @param mixed $peso
+     */
+    public function setPeso($peso)
+    {
+        $this->peso = $peso;
+
+        $this->setCategoria();
+
+
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategoria()
+    {
+        return $this->categoria;
+    }
+
+    /**
+     * @param mixed $categoria
+     */
+    public function setCategoria()
+
+
+    {
+
+        if($this->getPeso()<52.2){
+            echo'Invalido';
+        }
+        if ($this->getPeso()<=70.3){
+
+            $this->setCategoria('Leve');
+
+        }
+        if ($this->getPeso()<=83.9){
+
+            $this->setCategoria('Medio');
+
+        }
+        if ($this->getPeso()<=120.2){
+
+            $this->setCategoria('Pesado');
+
+        }
+        else{
+            echo'Invalido';
+
+
+        }
+
+    }
+
+    /**
+     * @return string
+     */
+    public function getVitorias()
+    {
+        return $this->vitorias;
+    }
+
+    /**
+     * @param string $vitorias
+     */
+    public function setVitorias($vitorias)
+    {
+        $this->vitorias = $vitorias;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDerrotas()
+    {
+        return $this->derrotas;
+    }
+
+    /**
+     * @param string $derrotas
+     */
+    public function setDerrotas($derrotas)
+    {
+        $this->derrotas = $derrotas;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmpates()
+    {
+        return $this->empates;
+    }
+
+    /**
+     * @param string $empates
+     */
+    public function setEmpates($empates)
+    {
+        $this->empates = $empates;
+    }
+
+
+
+
+
+    public function apresentar()
+
+    {
+
+        echo '<p> ------ Apresentacao ---- </p>';
+        echo '<p>Lutador:'.$this->getNome().'</p>';
+        echo '<p>Origem:'.$this->getNacionalidade().'</p>';
+        echo '<p>Idade:'.$this->getIdade().'</p>';
+        echo '<p>Pesando:'.$this->getPeso().'</p>';
+        echo '<p>Ganhou:'.$this->getVitorias().'</p>';
+        echo '<p>Perdeu:'.$this->getDerrotas().'</p>';
+        echo '<p>Empatou:'.$this->getEmpates().'</p>';
+
+
+    }
+
+    public function status()
+    {
+        echo '<p> ------ Status ---- </p>';
+        echo '<p>Lutador:'.$this->getNome().'</p>';
+        echo '<p>Pesando:'.$this->getPeso().'Categoria:'.$this->getCategoria().'</p>';
+        echo '<p>Ganhou:'.$this->getVitorias().'</p>';
+        echo '<p>Perdeu:'.$this->getDerrotas().'</p>';
+        echo '<p>Empatou:'.$this->getEmpates().'</p>';
+    }
+
+    public function ganharLuta()
+    {
+        $this->setVitorias($this->getVitorias() + 1);
+    }
+
+    public function perderLuta()
+    {
+       $this->setDerrotas($this->getDerrotas() + 1);
+    }
+
+    public function empatarLuta()
+    {
+        $this->setEmpates($this->getEmpates() + 1);
+    }
+}
---
+++ Lutador/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2//EN">
+<!--
+*        Sample comment
+-->
+<HTML>
+<head>
+    <title>PhpStorm</title>
+</head>
+<body>
+<pre>
+<?php
+
+require_once  'Lutador.php';
+
+$c = new Lutador();
+
+$c->setPeso(80);
+
+print_r ($c);
+
+
+?>
+    </pre>
+</body>
+</html>
